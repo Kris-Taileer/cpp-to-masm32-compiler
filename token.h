1232b4ad262c7244f46715f5f0083204e035e295
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2025-01-16 21:33:59 +0300

    Update token.h
    finished identify function

--- token.h
+++ token.h
@@ -97,6 +97,10 @@ public:
             return token_type::INT;
         }
 
+        if (lexeme == "const") {
+            return token_type::CONST;
+        }
+
         if (lexeme == "(") {
             return token_type::LPAR;
         }
@@ -141,7 +145,200 @@ public:
             return token_type::AUTO;
         }
 
-        //NEDDA FINISH  //yeah, new verb shortcut in english :D
+        if (lexeme == "for") {
+            return token_type::FOR;
+        }
+
+        if (lexeme == "while") {
+            return token_type::WHILE;
+        }
+
+        if (lexeme == "do") {
+            return token_type::DO_WHILE;
+        }
+
+        if (lexeme == "break") {
+            return token_type::BREAK;
+        }
+
+        if (lexeme == "continue") {
+            return token_type::CONTINUE;
+        }
+
+        if (lexeme == "if") {
+            return token_type::IF;
+        }
+
+        if (lexeme == "else") {
+            return token_type::ELSE;
+        }
+
+        if (lexeme == ">") {
+            return token_type::GREATER;
+        }
+
+        if (lexeme == "<") {
+            return token_type::LESS;
+        }
+
+        if (lexeme == "<=") {
+            return token_type::LESS_EQUAL;
+        }
+
+        if (lexeme == ">=") {
+            return token_type::GREATER_EQUAL;
+        }
+
+        if (lexeme == "==") {
+            return token_type::EQUAL;
+        }
+
+        if (lexeme == "!=") {
+            return token_type::NOT_EQUAL;
+        }
+
+        if (lexeme == "&&") {
+            return token_type::AND;
+        }
+
+        if (lexeme == "||") {
+            return token_type::OR;
+        }
+
+        if (lexeme == "!") {
+            return token_type::EXCLAMATION;
+        }
+
+        if (lexeme == "+") {
+            return token_type::PLUS;
+        }
+
+        if (lexeme == "-") {
+            return token_type::MINUS;
+        }
+
+        if (lexeme == "*") {
+            return token_type::STAR;
+        }
+
+        if (lexeme == "/") {
+            return token_type::SLASH;
+        }
+
+        if (lexeme == "++") {
+            return token_type::INC;
+        }
+
+        if (lexeme == "--") {
+            return token_type::DEC;
+        }
+
+        if (lexeme == "=") {
+            return token_type::ASSIGN;
+        }
+
+        if (lexeme == "+=") {
+            return token_type::ADD_ASSIGN;
+        }
+
+        if (lexeme == "-=") {
+            return token_type::SUB_ASSIGN;
+        }
+
+        if (lexeme == "*=") {
+            return token_type::MUL_ASSIGN;
+        }
+
+        if (lexeme == "/=") {
+            return token_type::DIV_ASSIGN;
+        }
+
+        if (lexeme == "return") {
+            return token_type::RETURN;
+        }
+
+        if (lexeme == "new") {
+            return token_type::NEW;
+        }
+
+        if (lexeme == "delete") {
+            return token_type::DELETE;
+        }
+
+        if (lexeme == "true") {
+            return token_type::TRUE;
+        }
+
+        if (lexeme == "false") {
+            return token_type::FALSE;
+        }
+
+        if (lexeme == "switch") {
+            return token_type::SWITCH;
+        }
+
+        if (lexeme == "case") {
+            return token_type::CASE;
+        }
+
+        if (lexeme == "default") {
+            return token_type::DEFAULT;
+        }
+
+        if (lexeme == ";") {
+            return token_type::SEMICOLON;
+        }
+
+        if (lexeme == ":") {
+            return token_type::COLON;
+        }
+
+        if (lexeme == ",") {
+            return token_type::COMMA;
+        }
+
+        if (lexeme == ".") {
+            return token_type::POINT;
+        }
+
+        if (lexeme == "?") {
+            return token_type::QUESTION;
+        }
+
+        if (lexeme == "::") {
+            return token_type::ACCESS_OPERATOR;
+        }
+
+        if (lexeme == "&") {
+            return token_type::AMPERSAND;
+        }
+
+        if (lexeme == "//") {
+            return token_type::LINE_COMMENT;
+        }
+
+        if (lexeme[0] == '#') {
+            return token_type::PREPROCESSOR_DIRECTIVE;
+        }
+
+        if (is_integer(lexeme)) {
+            return token_type::INTEGER_CONST;
+        }
+
+        if (is_double(lexeme)) {
+            return token_type::DOUBLE_CONST;
+        }
+
+        if (is_string(lexeme)) {
+            return token_type::STRING_CONST;
+        }
+
+        if (is_char(lexeme)) {
+            return token_type::CHAR_CONST;
+        }
+
+        return token_type :: IDENTIFIER;
+        //DONE!!!
     }
 
     bool is_string(const string& lexeme) {
