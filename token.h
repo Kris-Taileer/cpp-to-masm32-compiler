b3fe91c68c5b0feb6fe3b741ba0137c981e87169
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2025-01-18 11:53:29 +0300

    token.h DONE!!
    finished ;)

--- token.h
+++ token.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include<iostream>
 
 using namespace std;
 
@@ -89,6 +90,10 @@ public:
     token(const string& lexeme) {
         this ->lexeme = lexeme;
         this ->type = whar_token_type(lexeme);
+    }
+
+    void print() {
+        cout << lexeme << " type:" << token_type_to_str(type) << "\n"; //(int) == turn into int
     }
 
     token_type whar_token_type(const string& lexeme) { //fuck this shit //yandere dev code be like:
@@ -350,28 +355,168 @@ public:
     }
 
     bool is_integer(const string& lexeme) {
-        for (int i = 0; i < lexeme.size(); ++i) {
-            if ((lexeme[i] < '0') || (lexeme[i] > '9')) {
+        for (const char i: lexeme) {
+            if ((i < '0') || (i > '9')) {
                 return false;
             }
-            return true;
+
         }
+        return true;
     }
     bool is_double(const string& lexeme) {
-        bool flag_point = false;
-        for (int i = 0; i < lexeme.size(); ++i) {
-            if (lexeme[i] == '.') {
-                if (flag_point) {
-                    return false;
-                } else {
-                    flag_point = true;
-                }
+        bool point = false;
+        for (const char i: lexeme)
+        {
+            if (i == '.' && !point)
+            {
+                point = true;
+                continue;
             }
-            if ((lexeme[i] < '0') || (lexeme[i] > '9')) {
+            if (i == '.')
                 return false;
-            }
-            return true;
+            if (i < '0' || i > '9')
+                return false;
         }
+        return true;
+
+    }
+
+    string token_type_to_str(token_type t) {
+        switch (t) {
+        case token_type::IDENTIFIER:
+            return "Identifier";
+        case token_type::INTEGER_CONST:
+            return "integer constant";
+        case token_type::DOUBLE_CONST:
+            return "double constant";
+        case token_type::STRING_CONST:
+            return "string constant";
+        case token_type::CHAR_CONST:
+            return "char constant";
+        case token_type::TRUE:
+            return "true";
+        case token_type::FALSE:
+            return "false";
+        case token_type::CONST:
+            return "const";
+        case token_type::UNDEFINED:
+            return "undefined";
+        case token_type::INT:
+            return "int";
+        case token_type::DOUBLE:
+            return "double";
+        case token_type::BOOL:
+            return "bool";
+        case token_type::CHAR:
+            return "char";
+        case token_type::VOID:
+            return "void";
+        case token_type::AUTO:
+            return "auto";
+        case token_type::DO_WHILE:
+            return "do while";
+        case token_type::WHILE:
+            return "while";
+        case token_type::FOR:
+            return "for";
+        case token_type::BREAK:
+            return "break";
+        case token_type::CONTINUE:
+            return "continue";
+        case token_type::SWITCH:
+            return "switch";
+        case token_type::CASE:
+            return "case";
+        case token_type::DEFAULT:
+            return "default";
+        case token_type::IF:
+            return "if";
+        case token_type::ELSE:
+            return "else";
+        case token_type::LESS:
+            return "less";
+        case token_type::GREATER:
+            return "greater";
+        case token_type::LESS_EQUAL:
+            return "less and equal";
+        case token_type::GREATER_EQUAL:
+            return "greater and equal";
+        case token_type::EQUAL:
+            return "equal";
+        case token_type::NOT_EQUAL:
+            return "not equal";
+        case token_type::AND:
+            return "logic and";
+        case token_type::OR:
+            return "logic or";
+        case token_type::EXCLAMATION:
+            return "exclamation";
+        case token_type::PLUS:
+            return "plus";
+        case token_type::MINUS:
+            return "minus";
+        case token_type::STAR:
+            return "star";
+        case token_type::SLASH:
+            return "true";
+        case token_type::INC:
+            return "inc";
+        case token_type::DEC:
+            return "dec";
+        case token_type::LBRA:
+            return "lbra";
+        case token_type::RBRA:
+            return "rbra";
+        case token_type::LPAR:
+            return "lpar";
+        case token_type::RPAR:
+            return "rpar";
+        case token_type::LSQR:
+            return "lsqr";
+        case token_type::RSQR:
+            return "rsqr";
+        case token_type::ASSIGN:
+            return "assign";
+        case token_type::ADD_ASSIGN:
+            return "add assign";
+        case token_type::SUB_ASSIGN:
+            return "sun assign";
+        case token_type::MUL_ASSIGN:
+            return "mul assign";
+        case token_type::DIV_ASSIGN:
+            return "true";
+        case token_type::FUNCTION:
+            return "function";
+        case token_type::RETURN:
+            return "return";
+        case token_type::SEMICOLON:
+            return "semicolon";
+        case token_type::COLON:
+            return "colon";
+        case token_type::COMMA:
+            return "comma";
+        case token_type::POINT:
+            return "point";
+        case token_type::QUESTION:
+            return "question";
+        case token_type::AMPERSAND:
+            return "ampersand";
+        case token_type::LINE_COMMENT:
+            return "line comment";
+        case token_type::NEW:
+            return "new";
+        case token_type::DELETE:
+            return "delete";
+        case token_type::PREPROCESSOR_DIRECTIVE:
+            return "preprocessor directive";
+        case token_type::ACCESS_OPERATOR:
+            return "access operator";
+
+        default:
+            return "";
+
+        }
+
     }
 
     //bool is_long(const string& lexeme) {
